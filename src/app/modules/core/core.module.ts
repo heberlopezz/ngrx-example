1fd3e61aaa9c5017c8509ba4fbf7bcb5a1fcb0b5
heberlopezz <heberjlg@gmail.com>
2020-06-20 17:59:29 -0600

    create header for application

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const components = [LayoutComponent];
 
 @NgModule({
-  declarations: [...components],
-  imports: [CommonModule, RouterModule],
+  declarations: [...components, HeaderComponent],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
   exports: [...components],
 })
 export class CoreModule {}
